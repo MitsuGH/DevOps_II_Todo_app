9557c869ce400b788154b63e0127c27482276deb
Mitsu <mitsu.sanpen@gmail.com>
2025-02-04 01:59:16 +0700

    "added features"

--- searching-product/app.js
+++ searching-product/app.js
@@ -2,45 +2,37 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 
-
 const app = express();
-
 
 const PORT = 3000;
 
-
-//Mysql connection
+// MySQL connection
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password:'MitsuSQL5847',
+    password: 'MitsuSQL5847',
     database: 'product',
     port: '3306'
 });
 
-
 db.connect((err) => {
     if (err) throw err;
-    console.log('connection to Mysql database');
+    console.log('Connected to MySQL database');
 });
 
-
-//Middleware
-app.use(bodyParser.urlencoded({extended: true}));
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-
-//Route
-app.get('/',(req, res) => {
-    res.render('index', {product: [], query: '' })
-})
-
+// Routes
+app.get('/', (req, res) => {
+    res.render('index', { product: [], query: '' });
+});
 
 app.post('/search', (req, res) => {
     const query = req.body.query;
-    const sql = `SELECT * FROM product WHERE name LIKE ? or description LIKE ?`; //Like is mean path of data and use with %%
-
+    const sql = `SELECT * FROM products WHERE name LIKE ? OR description LIKE ?`;
 
     db.query(sql, [`%${query}%`, `%${query}%`], (err, results) => {
         if (err) throw err;
@@ -48,24 +40,63 @@ app.post('/search', (req, res) => {
     });
 });
 
-
 app.get('/add', (req, res) => {
     res.render('add-product');
 });
 
+app.post('/add', (req, res) => {
+    const { name, price, description } = req.body;
+    const sql = `INSERT INTO products (name, price, description) VALUES (?, ?, ?)`;
 
-app.post('/add-product', (req,res) => {
-    const{pname,price,desc} = req.body;
-    const sql =`INSERT INTO product (name,price,description) values (?,?,?)`;
-    db.query(sql,[pname,price,desc], (err,results) => {
+    db.query(sql, [name, price, description], (err, results) => {
         if (err) throw err;
         console.log("Product added", results);
         res.redirect('/');
     });
 });
 
+app.get('/edit/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = `SELECT * FROM products WHERE id = ?`;
 
-//Start Server
+    db.query(sql, [id], (err, results) => {
+        if (err) {
+            console.error('Error fetching product:', err);
+            return res.status(500).send('Database error');
+        }
+
+        if (results.length === 0) {
+            return res.status(404).send('Product not found');
+        }
+
+        res.render('edit', { product: results[0] });
+    });
+});
+
+app.post('/edit/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, price, description } = req.body;
+    const sql = `UPDATE products SET name = ?, price = ?, description = ? WHERE id = ?`;
+
+    db.query(sql, [name, price, description, id], (err, result) => {
+        if (err) throw err;
+        console.log("Product updated", result);
+        res.redirect('/');
+    });
+});
+
+app.post('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = `DELETE FROM products WHERE id = ?`;
+
+    db.query(sql, [id], (err, result) => {
+        if (err) throw err;
+        console.log("Product deleted", result);
+        res.redirect('/');
+    });
+});
+
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
